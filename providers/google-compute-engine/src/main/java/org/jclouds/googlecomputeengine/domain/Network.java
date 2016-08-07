0c51ed39650e9cab341136716a9d9732c63f7dd0
Nelson Araujo <nelsona@google.com>
2016-08-06 23:33:29 -0700

    JCLOUDS-1122: Support subnetworks definitions in Google Compute.

--- providers/google-compute-engine/src/main/java/org/jclouds/googlecomputeengine/domain/Network.java
+++ providers/google-compute-engine/src/main/java/org/jclouds/googlecomputeengine/domain/Network.java
@@ -18,17 +18,25 @@ package org.jclouds.googlecomputeengine.domain;
 
 import java.net.URI;
 import java.util.Date;
+import java.util.List;
 
 import org.jclouds.javax.annotation.Nullable;
 import org.jclouds.json.SerializedNames;
 
 import com.google.auto.value.AutoValue;
+import com.google.common.base.Strings;
 
 /**
  * Represents a network used to enable instance communication.
  */
 @AutoValue
 public abstract class Network {
+
+   public enum NetworkType {
+      LegacyNetwork,
+      AutoSubnetwork,
+      CustomNetwork,
+   }
 
    public abstract String id();
 
@@ -38,13 +46,15 @@ public abstract class Network {
 
    public abstract String name();
 
+   public abstract NetworkType type();
+
    @Nullable public abstract String description();
 
    /**
     * The range of internal addresses that are legal on this network. This range is a CIDR
     * specification, for example: {@code 192.168.0.0/16}.
     */
-   public abstract String rangeIPv4();
+   @Nullable public abstract String rangeIPv4();
 
    /**
     * This must be within the range specified by IPv4Range, and is typically the first usable address in that range.
@@ -52,10 +62,23 @@ public abstract class Network {
     */
    @Nullable public abstract String gatewayIPv4();
 
-   @SerializedNames({ "id", "creationTimestamp", "selfLink", "name", "description", "IPv4Range", "gatewayIPv4" })
+   @Nullable public abstract List<URI> subnetworks();
+
    public static Network create(String id, Date creationTimestamp, URI selfLink, String name, String description, String rangeIPv4,
-         String gatewayIPv4) {
-      return new AutoValue_Network(id, creationTimestamp, selfLink, name, description, rangeIPv4, gatewayIPv4);
+                                String gatewayIPv4) {
+      return new AutoValue_Network(id, creationTimestamp, selfLink, name, NetworkType.LegacyNetwork, description,
+              rangeIPv4, gatewayIPv4, null);
+   }
+
+   @SerializedNames({ "id", "creationTimestamp", "selfLink", "name", "description", "IPv4Range", "gatewayIPv4", "autoCreateSubnetworks", "subnetworks" })
+   public static Network create(String id, Date creationTimestamp, URI selfLink, String name, String description, String rangeIPv4,
+         String gatewayIPv4, String autoCreateSubnetworks, List<URI> subnetworks) {
+      NetworkType type;
+      type = !Strings.isNullOrEmpty(rangeIPv4) ? NetworkType.LegacyNetwork
+              : (autoCreateSubnetworks.equals("true") ? NetworkType.AutoSubnetwork
+                  : NetworkType.CustomNetwork);
+      return new AutoValue_Network(id, creationTimestamp, selfLink, name, type, description, rangeIPv4, gatewayIPv4,
+              subnetworks);
    }
 
    Network() {
